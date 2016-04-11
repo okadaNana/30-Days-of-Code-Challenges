2802a79b88b7a6e2540456c87cd148646915d574
Owen <xiaotu.dev@gmail.com>
2016-04-11 11:31:43 +0800

    Update Day_25.java

--- Day_25.java
+++ Day_25.java
@@ -5,22 +5,32 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		for (int k = 0; k < N; k++) {
+			int num = scan.nextInt();
+			
+			if (num == 1) {
+				System.out.println("Not prime");
+			} else if (num == 2) {
+				System.out.println("Prime");
+			} else {
+				int m = (int) Math.sqrt(num + 1);
+				boolean prime = true;
+				for (int i = 2; i <= m; i++) {
+					if (num % i == 0) {
+						prime = false;
+						break;
+					}
+				}
+				System.out.println(prime ? "Prime" : "Not prime");
+			}
+		}
+		
+		scan.close();
+	}
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        for (int k = 0; k < N; k++) {
-            int num = scan.nextInt();
-            
-            int count = 0;
-            for (int i = 1; i <= num; i++) {
-                if (num % i == 0) {
-                    count++;
-                }
-            }
-            System.out.println(count == 2 ? "Prime" : "Not prime");
-        }
-        
-        scan.close();
-    }
 }
