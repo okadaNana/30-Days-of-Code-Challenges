eae2a92478883949c9ce367cc6ecd1e0b202798f
Owen <xiaotu.dev@gmail.com>
2016-04-11 11:34:07 +0800

    Day 25: Running Time and Complexity

--- Day_25.java
+++ Day_25.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -5,32 +6,33 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
-	
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int N = scan.nextInt();
-		for (int k = 0; k < N; k++) {
-			int num = scan.nextInt();
-			
-			if (num == 1) {
-				System.out.println("Not prime");
-			} else if (num == 2) {
-				System.out.println("Prime");
-			} else {
-				int m = (int) Math.sqrt(num + 1);
-				boolean prime = true;
-				for (int i = 2; i <= m; i++) {
-					if (num % i == 0) {
-						prime = false;
-						break;
-					}
-				}
-				System.out.println(prime ? "Prime" : "Not prime");
-			}
-		}
-		
-		scan.close();
-	}
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int N = scan.nextInt();
+        for (int k = 0; k < N; k++) {
+            int num = scan.nextInt();
+            
+            if (num == 1) {
+                System.out.println("Not prime");
+            } else if (num == 2) {
+                System.out.println("Prime");
+            } else {
+                int m = (int) Math.sqrt(num + 1);
+                boolean prime = true;
+                for (int i = 2; i <= m; i++) {
+                    if (num % i == 0) {
+                        prime = false;
+                        break;
+                    }
+                }
+                System.out.println(prime ? "Prime" : "Not prime");
+            }
+        }
+        
+        scan.close();
+    }
 
 }
+
