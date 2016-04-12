b4aecb8775b68d503931434e6ec386030266f558
Owen <xiaotu.dev@gmail.com>
2016-04-12 11:21:44 +0800

    Day 26: Nested Logic v2

--- Day_26.java
+++ Day_26.java
@@ -1,9 +1,3 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
 public class Solution {
 
     public static void main(String[] args) {
@@ -17,18 +11,13 @@ public class Solution {
         int yearExpected = scan.nextInt();
         
         int hackos = 0;
-        if (yearReturn != yearExpected) {
+        
+        if (yearReturn > yearExpected) {
             hackos = 10000;
-        } else {
-            if (monthReturn > monthExpected) {
-                hackos = 500 * (monthReturn - monthExpected);
-            } else {
-                if (dayReturn > dayExpected) {
-                    hackos = 15 * (dayReturn - dayExpected);
-                } else {
-                    hackos = 0;
-                }
-            }
+        } else if (monthReturn > monthExpected) {
+            hackos = 500 * (monthReturn - monthExpected);
+        } else if (dayReturn > dayExpected) {
+            hackos = 15 * (dayReturn - dayExpected);
         }
         
         System.out.println(hackos);
