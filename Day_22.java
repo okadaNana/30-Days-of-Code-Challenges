4729e1ff85496dae1ed4aa0b64927b82d517a216
Owen <xiaotu.dev@gmail.com>
2016-04-15 10:41:10 +0800

    Day 22: Binary Search Trees v2

--- Day_22.java
+++ Day_22.java
@@ -1,10 +1,8 @@
 public static int getHeight(Node root){
-    if (root == null) {
-        return 0;
-    }
-    
-    return Math.max(
-        getHeight(root.left) + 1,
-        getHeight(root.right) + 1
-    );
+    int temp = temp(root);    
+    return (temp == 0) ? 0 : temp - 1;
+}
+
+public static int temp(Node root) {
+    return (root == null) ? 0 : 1 + Math.max(temp(root.left), temp(root.right));
 }
